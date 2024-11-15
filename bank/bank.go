a7406abe1b58ecb00f020a9219c1c83f679a49db
sholikul <sholikulardian@gmail.com>
2024-11-15 10:43:05 +0700

    added Deposit method to the Account struct

--- bank/bank.go
+++ bank/bank.go
@@ -1,5 +1,17 @@
 package bank
 
+import "errors"
+
 type Account struct {
 	Balance float64
+}
+
+func (a *Account) Deposit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount cannot be less than equal zero")
+	}
+
+	a.Balance += amount
+
+	return nil
 }
