d491a3ba2ac85108f4612275406fa9dddd704521
sholikul <sholikulardian@gmail.com>
2024-11-15 10:51:58 +0700

    implementation of bank import packages

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"hop-unit-testing/bank"
+)
+
+func main() {
+	account := bank.Account{}
+
+	// Deposit money
+	if err := account.Deposit(100.50); err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// Attempt to withdraw money
+	if err := account.Withdraw(50); err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// Attempt to withdraw more than the balance
+	if err := account.Withdraw(100); err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println("Final Balance:", account.GetBalance())
+}
