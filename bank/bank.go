3db3503754f58909b5b7a31c479e5fb4752d5843
sholikul <sholikulardian@gmail.com>
2024-11-15 10:43:44 +0700

    added Withdraw method to the Account struct

--- bank/bank.go
+++ bank/bank.go
@@ -15,3 +15,17 @@ func (a *Account) Deposit(amount float64) error {
 
 	return nil
 }
+
+func (a *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount cannot be less than equal zero")
+	}
+
+	if amount > a.Balance {
+		return errors.New("insufficient funds")
+	}
+
+	a.Balance -= amount
+
+	return nil
+}
