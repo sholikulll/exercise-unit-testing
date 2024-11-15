6f3636fcbc235dc46611d24183788b6396709fd6
sholikul <sholikulardian@gmail.com>
2024-11-15 17:52:33 +0700

    added test case Withdraw

--- bank/bank_test.go
+++ bank/bank_test.go
@@ -30,4 +30,24 @@ var _ = Describe("Bank", func() {
 			Expect(account.Balance).To(Equal(200.50))
 		})
 	})
+
+	Describe("WithDraw", func() {
+		It("should return an error if amount is less than or equal to zero", func() {
+			err := account.Withdraw(0)
+			Expect(err).ShouldNot(BeNil())
+			Expect(err.Error()).To(Equal("the amount cannot be less than equal zero"))
+		})
+
+		It("should return an error if amount is greater than balance", func() {
+			err := account.Withdraw(500)
+			Expect(err).ShouldNot(BeNil())
+			Expect(err.Error()).To(Equal("insufficient funds"))
+		})
+
+		It("should return the appropriate amount of account balance after withdraw", func() {
+			err := account.Withdraw(20.0)
+			Expect(err).Should(BeNil())
+			Expect(account.Balance).To(Equal(80.0))
+		})
+	})
 })
