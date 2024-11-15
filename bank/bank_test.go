53aacc4bc8e64292f993bfb2656f91e059028523
sholikul <sholikulardian@gmail.com>
2024-11-15 17:52:05 +0700

    added test case Deposit

--- bank/bank_test.go
+++ bank/bank_test.go
@@ -8,5 +8,26 @@ import (
 )
 
 var _ = Describe("Bank", func() {
+	var account bank.Account
 
+	BeforeEach(func() {
+		account.Balance = 100.0
+	})
+	AfterEach(func() {
+		account.Balance = 0
+	})
+
+	Describe("Deposit", func() {
+		It("should return an error if amount is less than or equal to zero", func() {
+			err := account.Deposit(0)
+			Expect(err).ShouldNot(BeNil())
+			Expect(err.Error()).To(Equal("the amount cannot be less than equal zero"))
+		})
+
+		It("should return the appropriate amount of account balance after deposit", func() {
+			err := account.Deposit(100.50)
+			Expect(err).Should(BeNil())
+			Expect(account.Balance).To(Equal(200.50))
+		})
+	})
 })
